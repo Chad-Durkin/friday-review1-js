a57f070d747b32c7f4bca83afada092bb7082d5c
Chad Durkin <Chaddurkin@gmail.com>
2017-03-17 10:18:00 -0700

    add api search functionality

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,10 @@
-
+var DocSearch = require('./../js/doctor.js').docSearchModule;
 $(document).ready(function() {
-
+    var currentDocSearchObject = new DocSearch();
     $("#search-button").click(function() {
         event.preventDefault();
         $('.showDoctors').empty();
         var medIssue = $('#med-input').val();
-        console.log(medIssue);
-    })
-})
+        currentDocSearchObject.searchDoc(medIssue);
+    });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,1 +1,21 @@
 
+var apiKey = require('./../.env').apiKey;
+
+DocSearch = function() {
+
+};
+
+DocSearch.prototype.searchDoc = function (medIssue) {
+    console.log(medIssue);
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
+        response.data.forEach(function(practice)
+        {
+            console.log(practice);
+            $(".showDoctors").append('<li> Location: ' + practice.practices.location_slug + ' Doctor: ' + practice.practices.name + ' Website: ' + practice.practices.website + ' Description: </li><br><p> ' + practice.practices.description + ' </p>');
+        });
+    }).fail(function(error) {
+        $('.showDoctors').text(error.responseJSON.message);
+    });
+};
+
+exports.docSearchModule = DocSearch;
